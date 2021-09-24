ce4828d4c3d68be937ad82c01c986eb115228f20
flootah <ed.saenz@protonmail.ch>
2021-09-23 19:21:33 -0700

    Work complete commit
    completed work on both programs.

--- Number Guessing Game/main.c
+++ Number Guessing Game/main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
 /*
 Number Guessing Game:
 - implement using a small state machine that runs on a while loop.
@@ -8,3 +13,81 @@ Number Guessing Game:
 - any state can quit by typing 'exit' as input
 - take user input as int and use comparisons to decide next state
 */
+int state = 0;      // current state
+int exitUI = 0;     // set to 1 to exit UI (and thus the program)
+int maxNum = 10;    // max number that can be picked, inclusive
+char input[1000];         // lastest input
+int option;         // int version of input
+int r;              // random number
+
+int main() {
+    // seed rng for numbers
+    srand(time(NULL));
+
+    while(!exitUI) {
+        switch(state){
+            case 0:
+                printf("Number Guessing Game:\n");
+                printf("Press 1 to play a game\n");
+                printf("Press 2 to change the max number\n");
+                printf("press 3 to quit\n");
+                scanf("%s", &input);
+                option = atoi(input);
+                if(option > 3 || option < 0) {
+                    printf("Invalid option!\n\n\n");
+                    break;
+                }   
+                if (option == 1) {
+                    // set random number for game
+                    r = (rand() % maxNum) + 1;
+                }
+
+                state = option;
+                break;
+            case 1:
+                // choose number between 1 - maxNum, inclusive
+                printf("Guess a number!\n");
+                scanf("%s", &input);
+                if(strcmp(input, "q") == 0) {
+                    state = 0;
+                    break;
+                }
+                option = atoi(input);
+                // check for entered q
+                if(option == r) {
+                    // correct
+                    printf("You win!!\n\n\n");
+                    state = 0;
+                    break;
+                } else if (option < r) {
+                    // too low
+                    printf("Too low... Try again\n");
+                    break;
+                } else if (option > r) {
+                    // too high
+                    printf("Too high... Try again\n");
+                    break;
+                }
+            break;
+            case 2:
+                printf("Settings: Max Number\n");
+                printf("Choose a new maximum number that can be guessed.\n");
+                printf("The current max number is: %d\n", maxNum);
+                scanf("%s", &input);
+                option = atoi(input);
+                if(option > INT_MAX || option <= 0) {
+                    printf("Invalid option!\n");
+                    break;
+                }
+                maxNum = option;
+                printf("New max number: %d\n", maxNum);
+                state = 0;
+            break;
+            case 3:
+            printf("Exiting...\n");
+            exitUI = 1;
+            break;
+        }
+
+    }
+}
--- Restaurant Bill/main.c
+++ Restaurant Bill/main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /*
 Restaurant Bill:
 - Prompt user for two floats, used for tax and tip percentages
@@ -5,3 +9,30 @@ Restaurant Bill:
 - use simple arithmetic to calculate tip, tax, and total
 - print to screen
 */
+
+float meals[4] = {9.95, 4.55, 13.25, 22.35};                    // prices of the 4 meals
+char mealnames[4][10] = {"Salad", "Soup", "Sandwich", "Pizza"}; // names of the 4 meals
+float taxPercentage = 0;                                        // tax percentage
+float tipPercentage = 0;                                        // tip percentage
+
+int main() {
+    // prompt for percentages
+    printf("Restaurant Bill Calculator:\n\n");
+    printf("Enter tax amount as percentage: ");
+    scanf("%f", &taxPercentage);
+    printf("Enter tip amount as percentage: ");
+    scanf("%f", &tipPercentage);
+
+    // create rng for meal
+    srand(time(NULL));
+    int r = rand() % 4;
+    // set meal price and determine tax, tip, and total
+    float meal = meals[r];
+    float tax = meal * taxPercentage * 0.01;
+    float tip = meal * tipPercentage * 0.01;
+    float total = meal + tax + tip;
+    printf("%10s", mealnames[r]); printf(":"); printf("%6.2f\n", meal);
+    printf("%10s", "Tax:"); printf("%6.2f\n", tax);
+    printf("%10s", "Tip:"); printf("%6.2f\n", tip);
+    printf("%10s", "Total:"); printf("%6.2f\n", total);
+}
