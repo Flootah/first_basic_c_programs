1a5a1ed4c40d9ecbb9a9b080f543a70dab4d134c
flootah <ed.saenz@protonmail.ch>
2021-09-23 17:42:55 -0700

    Initial commit

---
+++ Number Guessing Game/main.c
@@ -0,0 +1,10 @@
+/*
+Number Guessing Game:
+- implement using a small state machine that runs on a while loop.
+    - state 0, main menu
+    - state 1, play game
+    - state 2, settings (change max number)
+    - state 3, quit
+- any state can quit by typing 'exit' as input
+- take user input as int and use comparisons to decide next state
+*/
---
+++ Restaurant Bill/main.c
@@ -0,0 +1,7 @@
+/*
+Restaurant Bill:
+- Prompt user for two floats, used for tax and tip percentages
+- use rng to determine which of 4 meal costs to use
+- use simple arithmetic to calculate tip, tax, and total
+- print to screen
+*/
